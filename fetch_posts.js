7ab155d464c25ed9d9ad87e22b102ed82ccb307e
curist <curist.cyc@gmail.com>
2013-07-26 12:15:48 +0800

    Fetch post content and comments

--- fetch_posts.js
+++ fetch_posts.js
@@ -1,30 +1,34 @@
 var restler = require('restler');
+var AUTH = require('./auth_token');
 var _ = require('underscore');
+var when = require('when');
 
 var Post = require('./models/post');
 
 module.exports.update = function() {
   // TODO: support ETag
-  restler.json('https://api.github.com/users/curist/gists', {
-    headers: {
-      Authorization: 'token e4f95b9a510741dae14cbc0b353d3270f913d7b8'
-    }
-  })
+  restler.json('https://api.github.com/users/curist/gists', AUTH)
   .on('success', function(gists, resp) {
-    var posts = gists.filter(function(gist) {
+    var gists = gists.filter(function(gist) {
       return _(Object.keys(gist.files)).contains('blogpost.md');
-    }).map(function(gist) {
-      return prune(Post, new Post(gist));
     });
 
-    posts.forEach(function(post) {
-      Post.update({id: post.id}, post, {upsert: true}, function(err) {
-        if(err) {
-          console.log(err);
-        }
-      });
+    gists.forEach(function(gist) {
+      when.all([promise_get_post(gist), promise_get_comments(gist)])
+        .then(function(values) {
+          gist.post_content = values[0];
+          gist.comments = values[1];
+
+          var post = prune(new Post(gist));
+          // console.log(post);
+          Post.update({id: post.id}, post, {upsert: true}, function(err) {
+            if(err) {
+              console.log(err);
+            }
+          });
+          process.exit(0);
+        });
     });
-    process.exit(0);
   })
   .on('fail', function(data, resp) {
     console.log('Failed to update gists.');
@@ -36,14 +40,36 @@ module.exports.update = function() {
   });
 };
 
-function prune(model, instance) {
-  var keys = Object.keys(model.schema.paths).filter(
-    function(s){return s !== '_id' && s !== '__v'}
-  );
-  var obj = {};
-  keys.forEach(function(key) {
-    obj[key] = instance[key];
+function promise_get_post(gist) {
+  var post_url = gist.files['blogpost.md'].raw_url;
+  var deferred = when.defer();
+  restler.get(post_url, AUTH)
+  .on('success', function(body, resp) {
+    deferred.resolve(body);
   })
+  .on('error', function(err, resp) {
+    deferred.reject(err);
+  });
+  return deferred.promise;
+}
+
+function promise_get_comments(gist) {
+  var comments_url = gist.comments_url;
+  var deferred = when.defer();
+  restler.json(comments_url, AUTH)
+  .on('success', function(comments, resp) {
+    deferred.resolve(comments);
+  })
+  .on('error', function(err, resp) {
+    deferred.reject(err);
+  });
+  return deferred.promise;
+
+}
+
+function prune(instance) {
+  var obj = instance.toObject();
+  delete obj._id;
   return obj;
 }
 
--- models/post.js
+++ models/post.js
@@ -2,7 +2,21 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
   id: String,
   created_at: Date,
-  updated_at: Date
+  updated_at: Date,
+  description: String,
+  post_content: {type: String, default: ''},
+  comments: [{
+    id: String,
+    created_at: Date,
+    updated_at: Date,
+    user: {
+      login: String,
+      avatar_url: String,
+      html_url: String
+    },
+    body: String
+  }]
+
 });
 
 module.exports = mongoose.model('Post', schema);
