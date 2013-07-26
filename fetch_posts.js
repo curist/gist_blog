6a4fdaea8bd3f1c3bbf277a30ef49ddc795f6f5d
curist <curist.cyc@gmail.com>
2013-07-26 12:41:19 +0800

    Wrap gists fetching in when.all

--- fetch_posts.js
+++ fetch_posts.js
@@ -7,62 +7,64 @@ var Post = require('./models/post');
 
 module.exports.update = function() {
   // TODO: support ETag
-  restler.json('https://api.github.com/users/curist/gists', AUTH)
-  .on('success', function(gists, resp) {
-    var gists = gists.filter(function(gist) {
-      return _(Object.keys(gist.files)).contains('blogpost.md');
-    });
+  console.log('Updating posts...');
+  restler.get('https://api.github.com/users/curist/gists', AUTH)
+    .on('success', function(gists, resp) {
+      var gists = gists.filter(function(gist) {
+        return _(Object.keys(gist.files)).contains('blogpost.md');
+      });
 
-    gists.forEach(function(gist) {
-      when.all([promise_get_post(gist), promise_get_comments(gist)])
-        .then(function(values) {
-          gist.post_content = values[0];
-          gist.comments = values[1];
+      when.all(gists.map(function(gist) {
+        return when.all([promise_get_post(gist), promise_get_comments(gist)])
+          .then(function(values) {
+            gist.post_content = values[0];
+            gist.comments = values[1];
 
-          var post = prune(new Post(gist));
-          // console.log(post);
-          Post.update({id: post.id}, post, {upsert: true}, function(err) {
-            if(err) {
-              console.log(err);
-            }
+            var post = prune(new Post(gist));
+            // console.log(post);
+            Post.update({id: post.id}, post, {upsert: true}, function(err) {
+              if(err) {
+                console.log(err);
+              }
+            });
           });
-          process.exit(0);
+      }))
+        .then(function() {
+          console.log('Posts updated.');
         });
+    })
+    .on('fail', function(data, resp) {
+      console.log(data);
+      console.log('Failed to update gists.');
+    })
+    .on('error', function(err, resp) {
+      console.log('Err when updating gists: ' + err);
     });
-  })
-  .on('fail', function(data, resp) {
-    console.log('Failed to update gists.');
-    process.exit(1);
-  })
-  .on('error', function(err, resp) {
-    console.log('Err when updating gists: ' + err);
-    process.exit(2);
-  });
 };
 
 function promise_get_post(gist) {
   var post_url = gist.files['blogpost.md'].raw_url;
   var deferred = when.defer();
   restler.get(post_url, AUTH)
-  .on('success', function(body, resp) {
-    deferred.resolve(body);
-  })
-  .on('error', function(err, resp) {
-    deferred.reject(err);
-  });
+    .on('success', function(body, resp) {
+      deferred.resolve(body);
+    })
+    .on('error', function(err, resp) {
+      deferred.reject(err);
+    });
   return deferred.promise;
 }
 
 function promise_get_comments(gist) {
   var comments_url = gist.comments_url;
   var deferred = when.defer();
-  restler.json(comments_url, AUTH)
-  .on('success', function(comments, resp) {
-    deferred.resolve(comments);
-  })
-  .on('error', function(err, resp) {
-    deferred.reject(err);
-  });
+  restler.get(comments_url, AUTH)
+    .on('success', function(comments, resp) {
+      deferred.resolve(comments);
+    })
+    .on('error', function(err, resp) {
+      deferred.reject(err);
+    });
   return deferred.promise;
 
 }
--- test.js
+++ test.js
@@ -3,3 +3,7 @@ mongoose.connect('mongodb://localhost/gistblog');
 
 var fetch_posts = require('./fetch_posts');
 fetch_posts.update();
+
+setInterval(function() {
+  fetch_posts.update();
+}, 10000);
